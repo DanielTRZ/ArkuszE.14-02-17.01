f08052e07b7bb1424362818a997b9752eb4b3ed3
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2022-02-16 11:14:24 +0100

    Add files via upload

---
+++ samochody.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+  <head>
+     <meta charset="utf-8">
+     <title>Wynajmujemy Samochody</title>
+     <meta name="description" content="Opis zawartości strony dla wyszukiwarek">
+     <meta name="keywords" content="słowa, kluczowe, opisujące, zawartość">
+     <link rel="stylesheet" href="styl.css">
+  </head>
+  <body>
+
+ <div id="baner">
+     <h1>Wyanjem Samochodów</h1>
+ </div>
+ <div id="lewy">
+     <h2>DZIŚ POLECAMY TOYOTĘ ROCZNIK 2014</h2>
+     <?php
+     
+     
+     
+     
+     ?>
+     <h2>WSZYSTKIE DOSTĘPNE SAMOCHODY</h2>
+     <?php
+     
+     
+     
+     
+     ?>
+ </div>
+ <div id="srodkowy">
+     <h2>ZAMÓWIONE AUTA Z NUMERAMI TELEFONÓW KLIENTÓW</h2>
+     <?php
+     
+     
+     
+     ?>
+ </div>
+ <div id="prawy">
+   
+    <h2>NASZA OFERTA</h2>
+<ul>
+ 	<li>Fiat</li>
+ 	<li>Toyota</li>
+ 	<li>Opel</li>
+ 	<li>Mercedes</li>
+</ul>
+    Tu pobierzesz naszą <a href="baza.txt"> bazę danych</a>
+     <p>autor strony:00000000000</p>
+ </div>
+  </body>
+</html>
