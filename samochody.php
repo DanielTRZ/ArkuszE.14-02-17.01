1a640ab19d7ce9269659a8ccabf891976d221cbf
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-05-06 05:12:09 +0200

    Update samochody.php

--- samochody.php
+++ samochody.php
@@ -43,7 +43,7 @@
        $connect=mysqli_connect('localhost','root','','wynajem')or die ("Błąd połączenia :".mysqli_error());
        $q1=mysqli_query($connect,'SELECT samochody.id, model, telefon FROM samochody JOIN zamowienia ON samochody.id = zamowienia.Samochody_id'); 
 						
-			while($data = mysqli_fetch_assoc($q1))
+       while($data = mysqli_fetch_assoc($q1))
 			{
 				echo '<ul>'.$data['id'].' '.$data['model'].' '.$data['telefon'].'</ul>';
 			}        
