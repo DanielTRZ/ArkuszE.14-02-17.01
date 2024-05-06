b8e567d4983c9fe51f6be0e8a09b092162955850
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-05-06 05:10:26 +0200

    Update samochody.php

--- samochody.php
+++ samochody.php
@@ -33,7 +33,7 @@
 						
      while($data = mysqli_fetch_assoc($q1))
      {
-				echo '<li>'.$data['id'].' '.$data['marka'].' '.$data['model'].' '.$data['rocznik'].'</li>';
+     echo '<li>'.$data['id'].' '.$data['marka'].' '.$data['model'].' '.$data['rocznik'].'</li>';
 			}
      ?>
  </div>
